98fc8fcab616723d4b14d58d07bc72ab9d1e04f4
Clint Fralick <cfralick@pandemiclabs.com>
2013-09-02 19:45:46 -0400

    add host to schema import command

--- realtime/recipes/dbsetup.rb
+++ realtime/recipes/dbsetup.rb
@@ -1,7 +1,7 @@
 node[:deploy].each do |app_name, deploy|
-   "mysql-create-table" do
-    command "/usr/bin/s3cmd --config=/srv/www/realtime/current/.s3cfg get #{deploy[:mongo][:bucketnast]}/schema/realtime.sql; mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} < /srv/www/realtime/current/realtime.sql"
-    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:realtime][:dbtable]}"
+   execute "mysql-create-table" do
+    command "/usr/bin/s3cmd --config=/srv/www/realtime/current/.s3cfg get #{deploy[:database][:bucketnast]}/schema/realtime.sql; mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} < /srv/www/realtime/current/realtime.sql"
+    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:realtime][:dbtable]}"
     action :run
   end
 end
