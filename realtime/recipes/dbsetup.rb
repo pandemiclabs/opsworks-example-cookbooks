64eae8f1af181fc43ac258f37908d3d5bcf6e0e8
Clint Fralick <cfralick@pandemiclabs.com>
2013-09-02 20:04:08 -0400

    fail

--- realtime/recipes/dbsetup.rb
+++ realtime/recipes/dbsetup.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
    execute "mysql-create-table" do
-    command "/usr/bin/s3cmd --config=/srv/www/realtime/current/.s3cfg get s3://#{deploy[:database][:bucketnast]}/schema/realtime.sql; mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} < /srv/www/realtime/current/realtime.sql"
+    command "/usr/bin/s3cmd --config=/srv/www/realtime/current/.s3cfg get s3://#{deploy[:database][:bucketnast]}/schema/realtime.sql; mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} < realtime.sql"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:realtime][:dbtable]}"
     action :run
   end
