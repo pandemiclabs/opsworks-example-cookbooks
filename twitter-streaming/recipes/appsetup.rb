1190a69a760788c75c89baac5a6cda4511249f80
Clint Fralick <cfralick@pandemiclabs.com>
2013-09-05 09:59:43 -0400

    wrong var specified

--- twitter-streaming/recipes/appsetup.rb
+++ twitter-streaming/recipes/appsetup.rb
@@ -1,29 +1,5 @@
 node[:deploy].each do |app_name, deploy|
 
-  template "#{deploy[:deploy_to]}/current/config/mysql.js" do
-    source "mysql.js.erb"
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     (deploy[:database][:host] rescue nil),
-      :user =>     (deploy[:database][:username] rescue nil),
-      :password => (deploy[:database][:password] rescue nil),
-      :db =>       (deploy[:database][:database] rescue nil),
-      :port =>     (deploy[:database][:port] rescue nil),
-    )
-    
-   only_if do
-     File.directory?("#{deploy[:deploy_to]}/current")
-   end
-  end
-  
   template "#{deploy[:deploy_to]}/current/config/twitter.js" do
     source "twitter.js.erb"
     mode 0660
---
+++ twitter-streaming/recipes/mysqlsetup.rb
@@ -0,0 +1,24 @@
+template "#{deploy[:deploy_to]}/current/config/mysql.js" do
+    source "mysql.js.erb"
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")   
+      owner "apache"
+    end
+
+    variables(
+      :host =>     (deploy[:database][:host] rescue nil),
+      :user =>     (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db =>       (deploy[:database][:database] rescue nil),
+      :port =>     (deploy[:database][:port] rescue nil),
+    )
+    
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
+
