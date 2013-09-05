fe547ad0807482c00b766089c2686e19a01130fb
Clint Fralick <cfralick@pandemiclabs.com>
2013-09-05 02:33:05 -0400

    yay

--- realtime/recipes/dbsetup.rb
+++ realtime/recipes/dbsetup.rb
@@ -1,7 +1,15 @@
 node[:deploy].each do |app_name, deploy|
-   execute "mysql-create-table" do
-    command "/usr/bin/s3cmd --config=/srv/www/realtime/current/.s3cfg get s3://#{deploy[:database][:bucketnast]}/schema/realtime.sql; mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} < realtime.sql"
-    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:realtime][:dbtable]}"
-    action :run
-  end
+    script "mysql-create-schema" do
+        interpreter "bash"
+        user "root"
+        cwd "#{deploy[:deploy_to]}/current"
+        code <<-EOH
+        /usr/bin/s3cmd --config=#{deploy[:deploy_to]}/current/.s3cfg get s3://#{deploy[:database][:bucketnast]}/schema/#{deploy[:database][:schema]}
+        /usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} -h#{deploy[:database][:host]} < #{deploy[:database][:schema]}
+        EOH
+    end
+
+    not_if do
+        File.exists?("#{deploy[:deploy_to]}/current/#{deploy[:database][:schema]}")
+    end
 end
